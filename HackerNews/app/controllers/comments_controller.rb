502cfafce2588fac56174a0e33bc0d1ad40694df
Gabriela Stoykova <gabriela.ivanova.stoykova@est.fib.upc.edu>
2021-04-20 15:25:52 +0000

    comment tree for contributions/id

--- HackerNews/app/controllers/comments_controller.rb
+++ HackerNews/app/controllers/comments_controller.rb
@@ -27,7 +27,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: "Comment was successfully created." }
+        format.html { redirect_to Contribution.find_by(id: @comment.contr_id) , notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
